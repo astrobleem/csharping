d30d84b71d0b4a49ddfd1dc349e35aa118aa70d0
Chad Doebelin <chad.doebelin@gmail.com>
2022-01-13 14:17:07 -0600

    initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "What are you doing today?";
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+
+            Console.WriteLine("I'm fittin to do some C sharp");
+            Console.ReadKey();
+        }
+    }
+
+}
