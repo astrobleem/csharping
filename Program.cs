dd368a9b044313ed7bc50574a0d5b565014a87e1
Chad Doebelin <chad.doebelin@gmail.com>
2022-01-13 15:04:05 -0600

    working

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+ConsoleKeyInfo key;
+
+Console.Title = "What are you building today?";
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("C# stuff");
+bool running = true;
+
+do{
+
+
+key = Console.ReadKey(true);
+
+if (key.Key == ConsoleKey.DownArrow) 
+{
+Console.WriteLine("10 go 20");
+//running = false;
+}
+
+
+
+} while(key.Key != ConsoleKey.X);
+
+// Console.ReadKey();
 
